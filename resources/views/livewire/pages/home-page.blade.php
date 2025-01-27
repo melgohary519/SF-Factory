f529789f07202a173b1f20760d52cbc49373509f
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2025-01-27 21:15:06 +0200

    add home page icons

--- resources/views/livewire/pages/home-page.blade.php
+++ resources/views/livewire/pages/home-page.blade.php
@@ -1,17 +1,14 @@
 <div class="homepage mt-5 text-center">
     <div class="row mb-3 g-3">
-        <div class="col p-3">
-            <div>
-                <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
-                <span>فواتير الشراء</span>
-            </div>
-        </div>
+
         <div class="col">
             <div>
-                <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
-                <span>فواتير البيع</span>
+                <img src="{{ asset("images/icons/3.png") }}" class="img-thumbnail" alt="...">
+                <span>الصرفيات</span>
             </div>
         </div>
+
+        
         <div class="col">
             <div>
                 <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
@@ -20,45 +17,55 @@
         </div>
         <div class="col">
             <div>
-                <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
-                <span>الصرفيات</span>
+                <img src="{{ asset("images/icons/6.png") }}" class="img-thumbnail" alt="...">
+                <span>فواتير البيع</span>
             </div>
         </div>
+        <div class="col p-3">
+            <div>
+                <img src="{{ asset("images/icons/9.png") }}" class="img-thumbnail" alt="...">
+                <span>فواتير الشراء</span>
+            </div>
+        </div>
+        
+        
     </div>
 
     <div class="row mb-3">
         <div class="col">
             <div>
-                <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
-                <span>الارباح</span>
-            </div>
-        </div>
-        <div class="col">
-            <div>
-                <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
                 <span>الحوالات</span>
+                <img src="{{ asset("images/icons/7.png") }}" class="img-thumbnail" alt="...">
+            </div>
+        </div>
+        <div class="col">
+            <div>
+                <span>الارباح</span>
+                <img src="{{ asset("images/icons/5.png") }}" class="img-thumbnail" alt="...">
             </div>
         </div>
     </div>
 
     <div class="row mb-3">
+        
         <div class="col">
-            <div>
-                <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
-                <span>الموردين</span>
+            <div class="d-flex flex-column align-items-center">
+                <img src="{{ asset("images/icons/2.png") }}" class="img-thumbnail" alt="...">
+                <span>المواد</span>
             </div>
         </div>
         <div class="col">
-            <div>
-                <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
+            <div class="d-flex flex-column align-items-center">
+                <img src="{{ asset("images/icons/4.png") }}" class="img-thumbnail" alt="...">
                 <span>التجار</span>
             </div>
         </div>
         <div class="col">
-            <div>
-                <img src="{{ asset("images/icons/1.png") }}" class="img-thumbnail" alt="...">
-                <span>المواد</span>
+            <div class="d-flex flex-column align-items-center">
+                <img src="{{ asset("images/icons/8.png") }}" class="img-thumbnail" alt="...">
+                <span>الموردين</span>
             </div>
         </div>
+        
     </div>
 </div>
