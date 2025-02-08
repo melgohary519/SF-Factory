a7cfb2debe6e0d1251560d0adbbbe39c19f8487f
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2025-02-08 15:27:03 +0200

    SuplierAndTraderProfitPage

--- app/Livewire/ProfitsLosses/SuplierAndTraderProfitPage.php
+++ app/Livewire/ProfitsLosses/SuplierAndTraderProfitPage.php
@@ -2,15 +2,88 @@
 
 namespace App\Livewire\ProfitsLosses;
 
+use App\Models\Supplier;
+use App\Models\Trader;
 use Livewire\Component;
 
 class SuplierAndTraderProfitPage extends Component
 {
-    function mount($type) {
-        
+    public $data;
+    public $type;
+    public $pageTitle;
+    public $fromDate;
+    public $toDate;
+
+    public $total_priceIraqy;
+    public $total_priceDollary;
+    public $total_transferIraqy;
+    public $total_transferDollary;
+
+    function mount($type)
+    {
+        $this->type = $type;
+
+        if ($type == "supplier") {
+            $this->pageTitle = "كشف حساب المورد";
+
+        } elseif ($type == "trader") {
+            $this->pageTitle = "كشف حساب التاجر";
+        }
     }
     public function render()
     {
+        $this->reloadData();
+        session()->flash("page_name", $this->pageTitle);
         return view('livewire.profits-losses.suplier-and-trader-profit-page');
     }
+
+    public function updated()
+    {
+        $this->reloadData();
+    }
+
+
+    public function reloadData()
+    {
+        $this->data = [];
+        $this->total_priceIraqy = 0;
+        $this->total_priceDollary = 0;
+        $this->total_transferIraqy = 0;
+        $this->total_transferDollary = 0;
+
+        if ($this->type == "supplier") {
+            foreach (Supplier::all() as $suplier) {
+                $item = [
+                    "id" => $suplier->id,
+                    "name" => $suplier->name,
+                    "priceIraqy" => $suplier->invoices()->whereBetween('purchase_date', [$this->fromDate, $this->toDate])->sum("purchase_price"),
+                    "priceDollary" => $suplier->invoices()->whereBetween('purchase_date', [$this->fromDate, $this->toDate])->sum("dollar_value"),
+                    "transferIraqy" => $suplier->transfers()->whereBetween('transfer_date', [$this->fromDate, $this->toDate])->sum("amount"),
+                    "transferDollary" => $suplier->transfers()->whereBetween('transfer_date', [$this->fromDate, $this->toDate])->sum("dollar_value"),
+                ];
+                $this->data[] = $item;
+                $this->total_priceIraqy += $item['priceIraqy'];
+                $this->total_priceDollary += $item['priceDollary'];
+                $this->total_transferIraqy += $item['transferIraqy'];
+                $this->total_transferDollary += $item['transferDollary'];
+            }
+        } elseif ($this->type == "trader") {
+            foreach (Trader::all() as $trader) {
+            $item = [
+                "id" => $trader->id,
+                "name" => $trader->name,
+                "priceIraqy" => $trader->invoices()->whereBetween('sale_date', [$this->fromDate, $this->toDate])->sum("sale_price"),
+                "priceDollary" => $trader->invoices()->whereBetween('sale_date', [$this->fromDate, $this->toDate])->sum("dollar_value"),
+                "transferIraqy" => $trader->transfers()->whereBetween('transfer_date', [$this->fromDate, $this->toDate])->sum("amount"),
+                "transferDollary" => $trader->transfers()->whereBetween('transfer_date', [$this->fromDate, $this->toDate])->sum("dollar_value"),
+            ];
+            $this->data[] = $item;
+            $this->total_priceIraqy += $item['priceIraqy'];
+            $this->total_priceDollary += $item['priceDollary'];
+            $this->total_transferIraqy += $item['transferIraqy'];
+            $this->total_transferDollary += $item['transferDollary'];
+            }
+        }
+    }
+
 }
--- resources/views/livewire/profits-losses/suplier-and-trader-profit-page.blade.php
+++ resources/views/livewire/profits-losses/suplier-and-trader-profit-page.blade.php
@@ -1,3 +1,85 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="container-fluid itemspage mt-5">
+
+    <div class="row text-center">
+        
+    </div>
+
+
+    <div class="tableList p-3 ">
+        <form class="row g-3 mt-4 ms-3 me-3">
+
+            <label for="fromDate" class="d-flex align-items-center col form-label p-0">من تاريخ</label>
+            <input type="date" wire:model.live="fromDate" class="col form-control p-0 m-2" id="fromDate"
+                placeholder="من تاريخ">
+
+            <label for="toDate" class="d-flex align-items-center col form-label p-0 m-0">الي تاريخ</label>
+            <input type="date" wire:model.live="toDate" class="col form-control p-0 m-2" id="toDate"
+                placeholder="الي تاريخ">
+
+        </form>        
+    </div>
+
+    <div class="tableList table-responsive mt-4">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>اسم المورد</th>
+                    <th>السعر بالعراقي</th>
+                    <th>السعر بالدولار</th>
+                    <th>التحويل بالعراقي</th>
+                    <th>التحويل بالدولار</th>
+                    <th>الباقي بالعراقي</th>
+                    <th>الباقي بالدولار</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data as $item)
+                    <tr>
+                        <td>{{ $item['name'] }}</td>
+                        <td>{{ number_format($item['priceIraqy'], 2, '.', ',') }}</td>
+                        <td>{{ number_format($item['priceDollary'], 2, '.', ',') }}</td>
+                        <td>{{ number_format($item['transferIraqy'], 2, '.', ',') }}</td>
+                        <td>{{ number_format($item['transferDollary'], 2, '.', ',') }}</td>
+                        <td>{{ number_format($item['priceIraqy'] - $item['transferIraqy'], 2, '.', ',') }}</td>
+                        <td>{{ number_format($item['priceDollary'] - $item['transferDollary'], 2, '.', ',') }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="row containerGrroup mt-5">
+
+        <div class="col">
+            <div class="row">
+                <div class="col-6">مجموع السعر بالعراقي </div>
+                <div class="col highlight">{{ number_format($total_priceIraqy, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-6">مجموع السعر بالدولار </div>
+                <div class="col highlight">{{ number_format($total_priceDollary, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-6">مجموع التحويل بالعراقي </div>
+                <div class="col highlight">{{ number_format($total_transferIraqy, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-6">مجموع التحويل بالدولار </div>
+                <div class="col highlight">{{ number_format($total_transferDollary, 2, '.', ',') }}</div>
+            </div>
+        </div>
+        
+        <div class="col">
+            <div class="row">
+                <div class="col-6"> مجموع الباقي بالعراقي </div>
+                <div class="col highlight">{{ number_format($total_priceIraqy - $total_transferIraqy, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-6"> مجموع الباقي بالدولار</div>
+                <div class="col highlight">{{ number_format($total_priceDollary - $total_transferDollary, 2, '.', ',') }}</div>
+            </div>
+        </div>
+    </div>
+
+
 </div>
