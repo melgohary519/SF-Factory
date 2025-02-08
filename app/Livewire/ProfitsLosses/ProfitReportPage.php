53f163c2b1962f35542dd71e3b6dfe4b7904f04e
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2025-02-08 15:27:33 +0200

    ProfitReportPage

--- app/Livewire/ProfitsLosses/ProfitReportPage.php
+++ app/Livewire/ProfitsLosses/ProfitReportPage.php
@@ -2,12 +2,69 @@
 
 namespace App\Livewire\ProfitsLosses;
 
+use App\Models\Expense;
+use App\Models\Item;
+use App\Models\PurchaseInvoice;
+use App\Models\SalesInvoice;
 use Livewire\Component;
 
 class ProfitReportPage extends Component
 {
+    public $fromDate;
+    public $toDate;
+    public $itemTypes;
+    public $itemType;
+    public $partners;
+
+    public $purchasePriceIraqy = 0;
+    public $purchasePriceDollary = 0;
+    public $salePriceIraqy = 0;
+    public $salePriceDollary = 0;
+    public $expensesIraqy = 0;
+    public $expensesDollary = 0;
+
+    public $restIraqy = 0;
+    public $restDollary = 0;
+
     public function render()
     {
+        $this->itemTypes = Item::all();
+        $this->partners = Item::all();
+
+        $this->reloadData();
+        
+
         return view('livewire.profits-losses.profit-report-page');
+    }
+    function updated() {
+        $this->reloadData();
+    }
+    function reloadData(): void {
+
+        $item = Item::find($this->itemType);
+        $itemSearch = "%";
+        if ($item != null) {
+            $itemSearch = $item->goods_type;
+        } 
+
+        $this->purchasePriceIraqy = PurchaseInvoice::whereBetween('purchase_date', [$this->fromDate, $this->toDate])
+            ->where('goods_type', 'like', $itemSearch)
+            ->sum("purchase_price");
+        $this->purchasePriceDollary = PurchaseInvoice::whereBetween('purchase_date', [$this->fromDate, $this->toDate])
+            ->where('goods_type', 'like', $itemSearch)
+            ->sum("dollar_rate");
+
+        $this->salePriceIraqy = SalesInvoice::whereBetween('sale_date', [$this->fromDate, $this->toDate])
+            ->where('goods_type', 'like', $itemSearch)
+            ->sum("sale_price"); 
+        $this->salePriceDollary = SalesInvoice::whereBetween('sale_date', [$this->fromDate, $this->toDate])
+            ->where('goods_type', 'like', $itemSearch)
+            ->sum("dollar_rate");
+
+        $this->expensesIraqy = Expense::whereBetween('expense_date', [$this->fromDate, $this->toDate])->sum("purchase_price");
+        $this->expensesDollary = Expense::whereBetween('expense_date', [$this->fromDate, $this->toDate])->sum("dollar_rate");
+
+        $this->restIraqy = ($this->purchasePriceIraqy + $this->expensesIraqy) - $this->salePriceIraqy;
+        $this->restDollary = ($this->purchasePriceDollary + $this->expensesDollary) - $this->salePriceDollary;
     }
 }
--- resources/views/livewire/profits-losses/profit-report-page.blade.php
+++ resources/views/livewire/profits-losses/profit-report-page.blade.php
@@ -1,3 +1,70 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="container-fluid itemspage mt-5">
+
+
+
+    <div class="tableList p-3 ">
+        <form class="row g-3 mt-4 ms-3 me-3">
+
+            <label for="fromDate" class="d-flex align-items-center col form-label p-0">من تاريخ</label>
+            <input type="date" wire:model.live="fromDate" class="col form-control p-0 m-2" id="fromDate"
+                placeholder="من تاريخ">
+
+            <label for="toDate" class="d-flex align-items-center col form-label p-0 m-0">الي تاريخ</label>
+            <input type="date" wire:model.live="toDate" class="col form-control p-0 m-2" id="toDate"
+                placeholder="الي تاريخ">
+
+            <label for="itemType" class="d-flex align-items-center col form-label p-0 m-0">نوع البضاعة</label>
+            <select wire:model.live="itemType" class="col form-control p-0 m-2" id="itemType">
+                <option value="">  الجميع</option>
+                @foreach($itemTypes as $type)
+                    <option value="{{ $type->id }}">{{ $type->goods_type }}</option>
+                @endforeach
+            </select>
+
+
+        </form>
+
+        <div class="containerGrroup mt-5">
+            <div class="row">
+                <div class="col-4">سعر الشراء بالعراقي</div>
+                <div class="col highlight">{{ number_format($purchasePriceIraqy, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">سعر الشراء بالدولار</div>
+                <div class="col highlight">{{ number_format($purchasePriceDollary, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">سعر البيع بالعراقي</div>
+                <div class="col highlight">{{ number_format($salePriceIraqy, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">سعر البيع بالدولار</div>
+                <div class="col highlight">{{ number_format($salePriceDollary, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">المصاريف بالعراقي</div>
+                <div class="col highlight">{{ number_format($expensesIraqy, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">المصاريف بالدولار</div>
+                <div class="col highlight">{{ number_format($expensesDollary, 2, '.', ',') }}</div>
+            </div>
+            
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col-4">الباقي بالعراقي</div>
+                <div class="col highlight">{{ number_format($restIraqy, 2, '.', ',') }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">الباقي بالدولار</div>
+                <div class="col highlight">{{ number_format($restDollary, 2, '.', ',') }}</div>
+            </div>
+        </div>
+
+    </div>
+
+
 </div>
