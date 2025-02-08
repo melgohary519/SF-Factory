b58f886a66fe001402742a2bcd5e6bd676f0b932
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2025-02-08 16:28:24 +0200

    MaterialReportPage

--- app/Livewire/ProfitsLosses/MaterialReportPage.php
+++ app/Livewire/ProfitsLosses/MaterialReportPage.php
@@ -2,12 +2,47 @@
 
 namespace App\Livewire\ProfitsLosses;
 
+use App\Models\Item;
+use App\Models\PurchaseInvoice;
+use App\Models\SalesInvoice;
 use Livewire\Component;
 
 class MaterialReportPage extends Component
 {
+    public $fromDate;
+    public $toDate;
+    public $items;
+    public $item;
+    public $selectedItemName;
+
+    public $quantity = 0;
+    public $salesQuantity = 0;
+    public $purchaseQuantity = 0;
+    public $remainingStock = 0;
+
     public function render()
     {
+        $this->items = Item::all();
+        $this->reloadData();
+
+        session()->flash("page_name", "كشف المواد");
         return view('livewire.profits-losses.material-report-page');
+    }
+    function updated() {
+        $this->reloadData();
+    }
+    function reloadData(): void {
+        $this->selectedItemName = Item::find($this->item)->goods_type ?? "_";
+
+        $this->purchaseQuantity = PurchaseInvoice::whereBetween('purchase_date', [$this->fromDate, $this->toDate])
+            ->where('goods_type', 'like', $this->selectedItemName ?? "")
+            ->sum("weight");
+
+        $this->salesQuantity = SalesInvoice::whereBetween('sale_date', [$this->fromDate, $this->toDate])
+            ->where('goods_type', 'like', $this->selectedItemName ?? "")
+            ->sum("weight");
+
+        $this->remainingStock = $this->purchaseQuantity - $this->salesQuantity;
+
     }
 }
--- resources/views/livewire/profits-losses/material-report-page.blade.php
+++ resources/views/livewire/profits-losses/material-report-page.blade.php
@@ -1,3 +1,50 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div class="container-fluid itemspage mt-5">
+
+
+
+    <div class="tableList p-3 ">
+        <form class="row g-3 mt-4 ms-3 me-3">
+
+            <label for="fromDate" class="d-flex align-items-center col form-label p-0">من تاريخ</label>
+            <input type="date" wire:model.live="fromDate" class="col form-control p-0 m-2" id="fromDate"
+                placeholder="من تاريخ">
+
+            <label for="toDate" class="d-flex align-items-center col form-label p-0 m-0">الي تاريخ</label>
+            <input type="date" wire:model.live="toDate" class="col form-control p-0 m-2" id="toDate"
+                placeholder="الي تاريخ">
+
+            <label for="item" class="d-flex align-items-center col form-label p-0 m-0">نوع البضاعة</label>
+            <select wire:model.live="item" class="col form-control p-0 m-2" id="itemType">
+                <option value="">  الجميع</option>
+                @foreach($items as $type)
+                    <option value="{{ $type->id }}">{{ $type->goods_type }}</option>
+                @endforeach
+            </select>
+
+
+        </form>
+
+        <div class="containerGrroup mt-5">
+            
+            <div class="row">
+                <div class="col-4">نوع البضاعة = </div>
+                <div class="col highlight">{{ $selectedItemName }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">الكمية = </div>
+                <div class="col highlight">{{ $purchaseQuantity }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">كمية البضاعة المباعة = </div>
+                <div class="col highlight">{{ $salesQuantity }}</div>
+            </div>
+            <div class="row">
+                <div class="col-4">كمية البضاعة المتبقية في المخزن = </div>
+                <div class="col highlight">{{ $remainingStock }}</div>
+            </div>
+        </div>
+
+    </div>
+
+
 </div>
