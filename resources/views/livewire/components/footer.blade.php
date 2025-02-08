db18f313b815ecf332d6b2600f9737f1ea95f499
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2025-02-08 15:28:52 +0200

    enhance footer repeate save your Money and add save your time

--- resources/views/livewire/components/footer.blade.php
+++ resources/views/livewire/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="container mt-5">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
       <div>
-        Save Your Money
+        Save Your Time
       </div>
       <div dir="ltr">
         @ 2024, made with 💜 by Visualize Team for a better solution
