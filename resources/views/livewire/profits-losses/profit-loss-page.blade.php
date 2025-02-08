803c6426c46e694ea24204f0b4195a07371abf3d
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2025-02-08 15:26:26 +0200

    enchance link text

--- resources/views/livewire/profits-losses/profit-loss-page.blade.php
+++ resources/views/livewire/profits-losses/profit-loss-page.blade.php
@@ -3,15 +3,15 @@
         <div class="row m-5">
             <div class="d-flex gap-3 justify-content-center buttons mt-5 ">
                 <a href="{{ route('profitlosses.material.report') }}" class="btn btn-success btn-lg bg4 p-4 w-50 border-0">كشف المخازن والمواد</a>
-                <a href="{{ route('profitlosses.supplierandtrader.account',['trader']) }}" class="btn btn-danger btn-lg p-4 bg4 w-50 border-0">كشف حساب التجار</a>
-                <a href="{{ route('profitlosses.supplierandtrader.account',['supplier']) }}" class="btn btn-danger btn-lg p-4 bg4 w-50 border-0">كشف حساب الموردين</a>
+                <a href="{{ route('profitlosses.supplierandtrader.account',['trader']) }}" class="btn btn-danger btn-lg p-4 bg4 w-50 border-0"> حساب التجار</a>
+                <a href="{{ route('profitlosses.supplierandtrader.account',['supplier']) }}" class="btn btn-danger btn-lg p-4 bg4 w-50 border-0"> حساب الموردين</a>
             </div>
         </div>
 
         <div class="row m-5">
             <div class="d-flex gap-3 justify-content-center buttons">
-                <a href="{{ route('profitlosses.supplierandtrader.profit',['trader']) }}" class="p-5 btn btn-success btn-lg w-100 bg4 border-0" style="font-size: 250%;"> حساب التاجر</a>
-                <a href="{{ route('profitlosses.supplierandtrader.profit',['supplier']) }}" class="p-5 btn btn-success btn-lg w-100 bg4 border-0" style="font-size: 250%;"> حساب المورد</a>
+                <a href="{{ route('profitlosses.supplierandtrader.profit',['trader']) }}" class="p-5 btn btn-success btn-lg w-100 bg4 border-0" style="font-size: 250%;">كشف حساب التاجر </a>
+                <a href="{{ route('profitlosses.supplierandtrader.profit',['supplier']) }}" class="p-5 btn btn-success btn-lg w-100 bg4 border-0" style="font-size: 250%;">كشف حساب المورد</a>
             </div>
         </div>
         
