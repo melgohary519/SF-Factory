eac8e635c94311f1098e85aa906bb60988b75960
Mohamed Elgohary <mohamed.elgohary.519@gmail.com>
2025-03-02 21:53:22 +0200

    add export material report data

--- app/Livewire/ProfitsLosses/MaterialReportPage.php
+++ app/Livewire/ProfitsLosses/MaterialReportPage.php
@@ -37,10 +37,12 @@ class MaterialReportPage extends Component
         session()->flash("page_name", "كشف المواد");
         return view('livewire.profits-losses.material-report-page');
     }
-    function updated() {
+    function updated()
+    {
         $this->reloadData();
     }
-    function reloadData(): void {
+    function reloadData(): void
+    {
         $this->selectedItemName = Item::find($this->item)->goods_type ?? "%";
 
         $this->purchaseQuantity = PurchaseInvoice::whereBetween('purchase_date', [$this->fromDate, $this->toDate])
@@ -58,5 +60,101 @@ class MaterialReportPage extends Component
 
         $this->remainingStock = $this->purchaseQuantity - $this->salesQuantity;
 
+    }
+
+
+    public function exportData()
+    {
+        $data = [];
+        $filename = '';
+
+        function sumArrayIndex($data, $index)
+        {
+
+            return array_sum(array_map(function ($item) use ($index) {
+                return isset($item[$index]) && !is_string($item[$index]) ? $item[$index] : 0;
+            }, $data));
+        }
+
+
+        $data[] = ['كشف  المواد'];
+        $data[] = ["الفترة من", $this->fromDate];
+        $data[] = ["الي", $this->toDate];
+        $data[] = [];
+        $data[] = [
+            "رقم",
+            "نوع البضاعة",
+            "الكمية المشتراة",
+            "اسم المخزن",
+            "اسم الشريك",
+            "تاريخ الشراء",
+            "كمية المواد المباعة",
+            "الكمية المتبقية في المخزن",
+        ];
+
+
+        $invoicesData = PurchaseInvoice::whereBetween('purchase_date', [$this->fromDate, $this->toDate])->get();
+        $invoicesDataCount = PurchaseInvoice::whereBetween('purchase_date', [$this->fromDate, $this->toDate])->count();
+        for ($i = 0; $i < $invoicesDataCount; $i++) {
+            $d = $invoicesData[$i];
+            $salesCount = SalesInvoice::whereBetween('sale_date', [$this->fromDate, $this->toDate])
+                ->where('goods_type', '=', $d->goods_type)
+                ->where('inventory_name', '=', $d->inventory_name)
+                ->sum("weight");
+
+            $data[] = [
+                $i + 1,
+                $d->goods_type,
+                $d->weight,
+                $d->inventory_name,
+                $d->partner_name,
+                $d->purchase_date,
+                $salesCount,
+                $d->weight - $salesCount,
+            ];
+        }
+
+        $data[] = [];
+            $data[] = [];
+            $data[] = [];
+            $data[] = [];
+            $data[] = [
+                '',
+                '',
+                sumArrayIndex($data,2),
+                '',
+                '',
+                '',
+                sumArrayIndex($data,6),
+                sumArrayIndex($data,7),
+
+                
+            ];
+            $data[] = [
+                '',
+                '',
+                'مجموع الحقول',
+                '',
+                '',
+                '',
+                'مجموع الحقول',
+                'مجموع الحقول',
+            ];
+
+
+
+       
+
+        // $filename = 'users_' . date('Ymd_His') . '.csv';
+        $filename = "كشف المواد_" . date('d_m_Y') . '.csv';
+        return response()->streamDownload(function () use ($data) {
+            $handle = fopen('php://output', 'w');
+
+            foreach ($data as $row) {
+                fputcsv($handle, $row);
+            }
+
+            fclose($handle);
+        }, $filename);
     }
 }
--- resources/views/livewire/profits-losses/material-report-page.blade.php
+++ resources/views/livewire/profits-losses/material-report-page.blade.php
@@ -70,7 +70,18 @@
             </div>
         </div>
 
+        
+            <div class="row m-5">
+                <div class="d-flex flex-column gap-3 justify-content-center ">
+                        <div class="row justify-content-center">
+                            <a href="#" style="background-color: #272E3A !important" class="p-3 rounded bg4 w-25" wire:click="exportData">طباعة كشف</a>   
+                            @if ($fromDate != null && $toDate != null)  
+                            @endif
+                        </div>
+                </div>
+            </div>
     </div>
+
 
 
 </div>
